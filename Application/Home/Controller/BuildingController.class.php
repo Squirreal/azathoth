eea12f9dc2da18cdc6a4f67db92f0a1b5372bedb
KMP <kmp@kmp-2.local>
2020-05-19 03:34:59 +0800

    [NF]seo

--- Application/Home/Controller/BuildingController.class.php
+++ Application/Home/Controller/BuildingController.class.php
@@ -62,6 +62,12 @@ class BuildingController extends BaseController {
         $id = I('get.id');
         $this->data['id'] = $id;
         $data = $this->weblogic->service(self::$TAG.'.'.__FUNCTION__, ['id' => $id]);
+
+        $data['keywords'] = "{$data['info']['name']}项目";
+        foreach($data['tags'] as $k => $v) {
+            $data['keywords'] .= $v.name;
+        }
+        $data['description'] = "松鼠国际房产(www.squirreal.cn)是一个精品国际房地产资产配置平台, 为您提供最专业, 最优质的服务。";
         if ($data['banner']) {
             $banner_video = '';
             $banner_images = [];
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -16,6 +16,9 @@ class IndexController extends BaseController {
 
     public function index() {
         $data = $this->weblogic->service(self::$TAG.'.index', []);
+
+        $data['keywords'] = "squirreal,海外房产,海外投资,海外买房,海外买房移民,买房移民,资产增值,资产回报,资产保值增值率,资产回报率";
+        $data['description'] = "松鼠国际房产(www.squirreal.cn)是一个精品国际房地产资产配置平台, 为您提供最专业, 最优质的服务。";
         $this->data = array_merge($this->data, $data);
 
         //热门楼盘
